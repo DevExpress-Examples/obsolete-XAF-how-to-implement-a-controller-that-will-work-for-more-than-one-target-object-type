2b5882b49090eaec4cebe8909ec0d07400a70bb8
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-23 16:29:48 +0300

    initial commit

---
+++ VB/WinWebSolution.Module/BO.vb
@@ -0,0 +1,33 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Xpo
+Imports DevExpress.Persistent.Base
+Imports DevExpress.Persistent.BaseImpl
+
+Namespace WinWebSolution.Module
+	Public Interface ISupportDemoAction
+	End Interface
+	<DefaultClassOptions, System.ComponentModel.DisplayName("I have the Demo Action")> _
+	Public Class DomainObject1
+		Inherits BaseObject
+		Implements ISupportDemoAction
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+	End Class
+	<DefaultClassOptions, System.ComponentModel.DisplayName("I have the Demo Action")> _
+	Public Class DomainObject2
+		Inherits BaseObject
+		Implements ISupportDemoAction
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+	End Class
+	<DefaultClassOptions, System.ComponentModel.DisplayName("I don't have the Demo Action")> _
+	Public Class DomainObject3
+		Inherits BaseObject
+		Public Sub New(ByVal session As Session)
+			MyBase.New(session)
+		End Sub
+	End Class
+End Namespace
